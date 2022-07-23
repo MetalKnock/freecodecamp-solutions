de1acb7b343282d81c8b8cc84be675e54646bd5f
MetalKnock <dimannisik1@gmail.com>
2022-07-23 22:41:38 +0700

    Solve DNA Pairing

---
+++ JavaScript Algorithms and Data Structures/dna-pairing.js
@@ -0,0 +1,33 @@
+/*
+
+Pairs of DNA strands consist of nucleobase pairs. Base pairs are represented by the characters AT and CG, which form building blocks of the DNA double helix.
+
+The DNA strand is missing the pairing element. Write a function to match the missing base pairs for the provided DNA strand. For each character in the provided string, find the base pair character. Return the results as a 2d array.
+
+For example, for the input GCG, return [["G", "C"], ["C","G"], ["G", "C"]]
+
+The character and its pair are paired up in an array, and all the arrays are grouped into one encapsulating array.
+*/
+
+function pairElement(str) {
+    let result=[];
+    for (let i = 0; i < str.length; i++) {
+      switch (str[i]){
+        case 'G':
+          result.push(['G','C']);
+          break;
+        case 'C':
+          result.push(['C','G']);
+          break;
+        case 'A':
+          result.push(['A','T']);
+          break;
+        case 'T':
+          result.push(['T','A']);
+          break;
+      }
+    }
+    return result;
+  }
+  
+  pairElement("GCG");
